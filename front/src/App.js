e5aaf76cbcba9ee57a2e9721841436e16e7f727b
ArtemMatv <46032437+ArtemMatv@users.noreply.github.com>
2021-03-18 16:22:47 +0200

    After merge fix commit with great changes

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 import React from "react";
-import {Route, Link, BrowserRouter} from 'react-router-dom';
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
+import {Route, BrowserRouter} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import Movie from './components/moviePage/Movie';
 import Booking from './components/booking/Booking'
 import RegistrationForm from "./components/accountLogs/RegistrationForm";
 import LoginForm from "./components/accountLogs/LoginForm";
+import Contacts from "./components/contacts/Contacts"
 
 function App() {
   return (
     <div className="App">
         <Header />
         <BrowserRouter>
+            {/* <Route exact path="/" component={Home} /> */}
+            <Route exact path="/contacts" component={Contacts} />
             <Route path='/login' component={LoginForm}/>
             <Route path='/registration' component={RegistrationForm}/>
             <Route path="/movies/:movieId" component={Movie} />
--- front/src/Components/Header/Header.js
+++ front/src/Components/Header/Header.js
@@ -3,19 +3,12 @@ import './Header.css'
 import {
     Navbar,
     Nav,
-    FormControl,
     Container,
     Form,
     Button
 } from "react-bootstrap";
-import Switch from "react-bootstrap/esm/Switch";
 import logo from "./logo192.png";
 import avatar from "./user128.png";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-import Home from "../Pages/Home";
-import Contacts from "../Pages/Contacts";
-import Booking from "../Pages/Booking";
 
 export default class Header extends Component {
     render (){
@@ -39,14 +32,11 @@ export default class Header extends Component {
                             <Nav.Link href="/"> Home </Nav.Link>
                             </div>
                             <div className="custom-nav">
-                            <Nav.Link href="/booking"> Booking </Nav.Link>
-                            </div>
-                            <div className="custom-nav">
                             <Nav.Link href="/contacts"> Contacts </Nav.Link>
                             </div>
                         </Nav>
                         <Form inline >
-                            <Button href="/registr" style={{marginRight: 10, backgroundColor: "#A64D4D"}} variant="secondary">Sign up</Button>
+                            <Button href="/registration" style={{marginRight: 10, backgroundColor: "#A64D4D"}} variant="secondary">Sign up</Button>
                             <Button href="/login" style={{marginRight: 10, backgroundColor: "#ABB4B4"}} variant="secondary">Sign in</Button>
                         </Form>
                         <Navbar>
@@ -63,14 +53,6 @@ export default class Header extends Component {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-
-            <Router>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/booking" component={Booking} />
-                    <Route exact path="/contacts" component={Contacts} />
-                </Switch>
-            </Router>
             </>
         )
     }
