17d0516b899cec285e769d0dd9d391ec9f53baf4
Vitaliy821 <46032498+Vitaliy821@users.noreply.github.com>
2021-03-22 12:03:15 +0200

    Now you can see users tickets

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
+import Ticket from '../Ticket/Ticket'
 import './Account.css'
-
+import moment from 'moment';
 //import BookedList from '../BookedList/BookedList'
 //import HistoryList from '../HistoryList/HistoryList'
 
@@ -10,14 +11,14 @@ class UserPage extends Component{
         this.state = {
             isLoaded: false,
             error: null,
-            id: 31,
-            user: {}
+            id: 2,
+            user: {},
+            tickets: []
         }
 
 
         
     }
-
 
 
     async errorHandler(response){
@@ -32,48 +33,62 @@ class UserPage extends Component{
    }
  
    componentDidMount() {
-       fetch('http://localhost:8081/users/'+this.state.id)
-           .then(this.errorHandler)
-           .then((result) => {
-                   console.log(result)
-                   this.setState({
-                       isLoaded: true,
-                       user: result
-                   }
-               );
-           })
-           .catch(err => {
-               this.setState({
-                   isLoaded: true,
-                   error: err
-               })
-           }
-       );
-   }
+    fetch('http://localhost:8081/users/'+this.state.id)
+        .then(this.errorHandler)
+        .then((result) => {
+                this.setState({
+                    isLoaded: true,
+                    user: result
+                }
+            );
+            fetch('http://localhost:8081/tickets/user/'+this.state.id)
+                .then(this.errorHandler)
+                .then(res=>{
+                    console.log(res)
+                    this.setState({
+                        tickets: res
+                    })
+                })
+                .catch(error => {
+                    this.setState({
+                        ticketsError: error
+                    })
+                
+            })
+        })
+        .catch(err => {
+            this.setState({
+                isLoaded: true,
+                error: err
+            })
+        }
+    );
+}
     
     render() {
         const {isLoaded, error, user} = this.state
+        const bookedTickets = this.state.tickets.filter(e =>(moment(e.date).format('hh:mm DD.MM.YY') > moment().format('hh:mm DD.MM.YY')))
+        //console.log(moment().format('hh:mm DD.MM.YY'))
+        const historyTickets = this.state.tickets.filter(e => moment(e.date).format('hh:mm DD.MM.YY') < moment().format('hh:mm DD.MM.YY'))
+        console.log(historyTickets)
+        console.log(bookedTickets)
         return(
             <div className="wrapper">
                 <div className="user_info__item">
                     <img className="user_img__item" src="https://cdn1.flamp.ru/6e8b1e5fe1cc01a4d15e1d27c602dfa7.jpg" alt='img'/>
-                    <div className="user_name__item">{user.first_name}</div>
+                    <div className="user_name__item">{user.firstName} {user.lastName}</div>
                 </div>
-                <div className="user_list__item">
-                            
-                            <div className="lists_wrapper__item">
-                                   <fieldset>
+                <div className="user_list__item"> 
+                                <fieldset className="lists_wrapper__item">
                                     <input  type="radio" name="sizeBy" value="weight" id="sizeWeight" checked="checked" />
                                     <label id="bookedlist_controller__item" className="radio_button__item" for="sizeWeight">Booked tickets</label>
                                    
                                     <input type="radio" name="sizeBy" value="dimensions" id="sizeDimensions" />
                                     <label id="historylist_controller__item"className="radio_button__item" for="sizeDimensions">History</label>
                                
-                                    <div id="booked_list__item">BookedList</div>
-                                    <div id="history_list__item">HistoryList</div>
+                                    <div id="booked_list__item" className="container"><div className="row"><Ticket tickets={bookedTickets} /></div></div>
+                                    <div id="history_list__item"className="container"><div className="row"><Ticket tickets={historyTickets} /></div></div>
                                 </fieldset>
-                            </div>
-                            
                 </div>
                 
             </div>
