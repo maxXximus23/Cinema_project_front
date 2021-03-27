e9b1fae6b0d5fd87badd71970a727369a7717853
Vitaliy821 <46032498+Vitaliy821@users.noreply.github.com>
2021-03-27 18:52:56 +0200

    Styling and debugin

--- src/components/backButton/BackButton.js
+++ src/components/backButton/BackButton.js
@@ -2,5 +2,5 @@ import React from "react";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 
 export default function BackButton(props) {
-    return <button onClick={props.backPath} className="movie-back-button"><AiOutlineArrowLeft style={{marginRight:"5px"}}/>Go back</button>
+    return <button onClick={props.backPath} className="movie-back-button"><AiOutlineArrowLeft/>Go back</button>
 }
