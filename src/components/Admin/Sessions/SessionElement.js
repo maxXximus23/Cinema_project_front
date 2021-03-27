8dba62096bb45c6e1f413573f24f7abbdb9b9bde
ArtemMatv <46032437+ArtemMatv@users.noreply.github.com>
2021-03-27 19:27:25 +0200

    Reworked error message on SessionElement

--- src/components/Admin/Sessions/SessionElement.js
+++ src/components/Admin/Sessions/SessionElement.js
@@ -73,54 +73,84 @@ class SessionElement extends React.Component {
     render() {
         const { session, error, reqiureSure, reqiureSureCancel } = this.state
 
-        if (error)
-            return <ErrorComponent error={error} />
-
         if (session==null)
             return <div></div>
 
-        return  <div className={session.isCanceled ? "session__item row session__canceled" : "session__item row"}>
-                    
-                    <div className="session__poster col-md-2">
-                        <img alt="" src={session.moviePoster}></img>
-                    </div>
-                    <label className="session__id col-md-1">{session.id}</label>
-                    <div className="session__movie col-md-2">
-                        <Link to={'/movies/' + session.movieId} style={{textDecoration:'none'}}>
-                            {session.movieTitle}
-                        </Link>
-                    </div>
-                    <div className="session__hall col-md-2">{session.hallName}</div>
-                    <div className={moment(session.date) < moment() ? "session__date col-md-2 session__outdated" : "session__date col-md-2"}>
-                        {moment(session.date).format("HH:mm DD-MM-YYYY")}
-                    </div>
-                    <div className="col-md-1 session__edit">
-                        <Link to={"/admin/sessions/" + session.id}
-                            className="session__edit__btn">
-                            <AiFillEdit/>
-                        </Link>
-                    </div>
-                    <div className="col-md-1 session__cancel">
-                        <div onClick={this.markToCancel}>
-                            {!reqiureSureCancel &&
-                                    <TiDelete className="session__cancel__btn"/>
-                            }
-                        </div>
-                        {reqiureSureCancel && 
-                            <button className="session__cancel__sure" onClick={this.cancelSession}>{session.isCanceled ? 'Activate' : 'Cancel'}</button>
-                        }        
-                    </div>
-                    <div className="col-md-1 session__remove" >
-                        <div onClick={this.markToDelete}>
-                            {!reqiureSure &&
-                                   <AiFillDelete className="session__remove__btn"/>
-                            }
-                        </div>
-                        {reqiureSure && 
-                            <button className="session__remove__sure" onClick={this.removeSession}>DELETE</button>
-                        }
-                    </div>
-                </div>
+        return (
+				<div className="all-session">
+					<div
+						className={
+							session.isCanceled
+								? 'session__item row session__canceled'
+								: 'session__item row'
+						}
+					>
+						<div className='session__poster col-md-2'>
+							<img alt='' src={session.moviePoster}></img>
+						</div>
+						<label className='session__id col-md-1'>{session.id}</label>
+						<div className='session__movie col-md-2'>
+							<Link
+								to={'/movies/' + session.movieId}
+								style={{ textDecoration: 'none' }}
+							>
+								{session.movieTitle}
+							</Link>
+						</div>
+						<div className='session__hall col-md-2'>
+							{session.hallName}
+						</div>
+						<div
+							className={
+								moment(session.date) < moment()
+									? 'session__date col-md-2 session__outdated'
+									: 'session__date col-md-2'
+							}
+						>
+							{moment(session.date).format('HH:mm DD-MM-YYYY')}
+						</div>
+						<div className='col-md-1 session__edit'>
+							<Link
+								to={'/admin/sessions/' + session.id}
+								className='session__edit__btn'
+							>
+								<AiFillEdit />
+							</Link>
+						</div>
+						<div className='col-md-1 session__cancel'>
+							<div onClick={this.markToCancel}>
+								{!reqiureSureCancel && (
+									<TiDelete className='session__cancel__btn' />
+								)}
+							</div>
+							{reqiureSureCancel && (
+								<button
+									className='session__cancel__sure'
+									onClick={this.cancelSession}
+								>
+									{session.isCanceled ? 'Activate' : 'Cancel'}
+								</button>
+							)}
+						</div>
+						<div className='col-md-1 session__remove'>
+							<div onClick={this.markToDelete}>
+								{!reqiureSure && (
+									<AiFillDelete className='session__remove__btn' />
+								)}
+							</div>
+							{reqiureSure && (
+								<button
+									className='session__remove__sure'
+									onClick={this.removeSession}
+								>
+									DELETE
+								</button>
+							)}
+						</div>
+					</div>
+					{error && <p className="error__message">{error.message}</p>}
+				</div>
+			)
     }
 }
 
