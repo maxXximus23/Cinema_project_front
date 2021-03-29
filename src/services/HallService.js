ae1721c7fe501eab8d1d1bd66ffeace4f812269d
mariayurchenko <maria.yurchenko17@gmail.com>
2021-03-29 20:57:29 +0300

    add manage halls

--- src/services/HallService.js
+++ src/services/HallService.js
@@ -11,11 +11,31 @@ class HallService{
             })
             .then(BaseService.handleError);
     }
+    static isNameFree = async (name)=>{
+        return fetch(BaseService._baseUrl+'/halls/check-' + name, {
+            headers: {
+                Authorization: AccountService.getToken()
+            }})
+            .then(response => {
+                if (!response.ok) {
+                    BaseService.handleResponseError(response);
+                }
+                return response.json();
+            })
+            .catch(error => {
+                BaseService.handleError(error);
+            });
+    }
+
 
     static getById= async (id) =>{
         console.log("HallService.getById(id):");
         console.log("id: " + id);
-        return fetch(BaseService._baseUrl+'/halls/'+id)
+        return fetch(BaseService._baseUrl+'/halls/'+id,
+            {
+                headers: {
+                    Authorization: AccountService.getToken()
+                }})
             .then(response => {
                 if (!response.ok) {
                     BaseService.handleResponseError(response);
@@ -30,15 +50,15 @@ class HallService{
         console.log("HallService.addHall(hall):");
         const requestOptions = {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(hall)
+            headers: { 'Content-Type': 'application/json' , Authorization: AccountService.getToken()},
+            body: JSON.stringify(hall),
         };
         return fetch(BaseService._baseUrl+'/halls', requestOptions)
             .then(response => {
                 if (!response.ok) {
                     BaseService.handleResponseError(response);
                 }
-                return response.json();
+                return response;
             })
             .catch(error => {
                 BaseService.handleError(error);
@@ -49,7 +69,8 @@ class HallService{
         console.log("id: " + id);
         const requestOptions = {
             method: 'DELETE',
-            headers: { 'Content-Type': 'application/json' }
+            headers: { 'Content-Type': 'application/json',
+                Authorization: AccountService.getToken()}
         };
         return fetch(BaseService._baseUrl+'/halls/' + id, requestOptions)
             .then(response => {
@@ -67,10 +88,84 @@ class HallService{
         console.log("id: " + id);
         const requestOptions = {
             method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
+            headers: { 'Content-Type': 'application/json',
+                Authorization: AccountService.getToken()
+             },
             body: JSON.stringify(hall)
         };
         return fetch(BaseService._baseUrl+'/halls/' + id, requestOptions)
+            .then(response => {
+                if (!response.ok) {
+                    BaseService.handleResponseError(response);
+                    return response;
+                }
+                return response;
+            })
+            .catch(error => {
+                BaseService.handleError(error);
+            });
+    };
+
+    static blockHall= async (id) =>{
+        console.log("HallService.blockHall(id):");
+        console.log("id: " + id);
+        const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' ,
+                Authorization: AccountService.getToken()}
+        };
+        return fetch(BaseService._baseUrl+'/halls/block/' + id, requestOptions)
+            .then(response => {
+                if (!response.ok) {
+                    BaseService.handleResponseError(response);
+                }
+                return response;
+            })
+            .catch(error => {
+                BaseService.handleError(error);
+            });
+    };
+    static unblockHall= async (id) =>{
+        console.log("HallService.unblockHall(id):");
+        console.log("id: " + id);
+        const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' ,
+                Authorization: AccountService.getToken()}
+        };
+        return fetch(BaseService._baseUrl+'/halls/unblock/' + id, requestOptions)
+            .then(response => {
+                if (!response.ok) {
+                    BaseService.handleResponseError(response);
+                }
+                return response;
+            })
+            .catch(error => {
+                BaseService.handleError(error);
+            });
+    };
+    static async getActive() {
+        console.log("HallService.getActive():");
+        return fetch(BaseService._baseUrl+'/halls/active',{
+            headers: {
+                Authorization: AccountService.getToken()
+            }})
+            .then(response => {
+                if (!response.ok) {
+                    BaseService.handleResponseError(response);
+                }
+                return response.json();
+            })
+            .catch(error => {
+                BaseService.handleError(error);
+            });
+    }
+    static async getBlocked() {
+        console.log("HallService.getBlocked():");
+        return fetch(BaseService._baseUrl+'/halls/blocked',{
+            headers: {
+                Authorization: AccountService.getToken()
+            }})
             .then(response => {
                 if (!response.ok) {
                     BaseService.handleResponseError(response);
